82727774bb4366dc2a66b902d9484c8446e1522c
ssannkkallpp <ssannkkallpp@gmail.com>
2020-12-05 08:57:34 +0530

    Update url_fetch.py

--- code/url_fetch.py
+++ code/url_fetch.py
@@ -3,12 +3,14 @@ from bs4 import BeautifulSoup
 
 l=[]
 
-ticker_name = 'TSLA'
+ticker_name = 'TSLA' # name of the ticker
 baseURL='https://www.marketwatch.com/search?q=' + ticker_name + '&m=Ticker&rpp=100&mp=0&bd=true&bd=false&bdv=05%2F31%2F2020&rs=true'
+# the base url set up to accumulate marketwatch articles under advanced search for the given ticker starting from May 31st (modifiable) going back all the way to 13 pages prior
+
 urls=[baseURL]
 for i in range(1,13):
     urls.append(baseURL+'&o='+str(i*100+1))
-# print urls
+# store urls in an array
 
 
 for u in urls:
@@ -18,5 +20,8 @@ for u in urls:
         x=link.get('href')
         if x.find('story')>=0 and x.find('http')>=0: l.append(x)
 
+    # collectively store the URLs of all the relevant web pages corresponding to the marketwatch articles.
 
 for x in l: print(x)
+
+# run in terminal and write onto a file named urls
